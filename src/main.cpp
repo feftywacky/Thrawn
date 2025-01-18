83d9997c9b8ae51a1c82d977e55001932e954334
feftywacky <linfeiyu14@gmail.com>
2025-01-18 14:25:20 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -16,20 +16,18 @@ int main() {
     thrawn::Position pos;
     init_all();
 
-    bool dev_mode = false;
-
+    bool dev_mode = true;
+    
     if (dev_mode)
     {
         cout<<"developer mode"<<endl;
-
-        thrawn::Position pos1;
-
-        parse_fen(pos1, "r3kb1r/3n1pp1/p6p/2pPp2q/Pp2N3/3B2PP/1PQ2P2/R3K2R w KQkq -");
+        parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        perft_test(pos,6);
+        // parse_fen("r3kb1r/3n1pp1/p6p/2pPp2q/Pp2N3/3B2PP/1PQ2P2/R3K2R w KQkq -");
         // parse_fen(position_2);
-        print_board(pos1, pos1.colour_to_move);
+        // print_board(colour_to_move);
 
-        //search_position(12);
-//        search_position_threaded(12,2);
+        // search_position(10);
 
     }
     else
